133c5229c2202dae32ad08c6cb2f1d8f08757314
Jimver <jim.verheijde@hotmail.com>
2017-04-28 20:26:20 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -f /data/deluged.pid
 
-openvpn --config /data/openvpn.conf file.conf auth-user-pass /data/password
+openvpn --config /data/openvpn.conf file.conf
 
 deluged -d -c /data -L info -l /data/deluged.log &
 deluge-web -c /data &
